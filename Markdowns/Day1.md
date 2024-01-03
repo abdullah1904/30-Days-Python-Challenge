2b7c878ed0a9637c8dc4ede304e3c4d3ee3cd985
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2024-01-04 02:25:11 +0500

    Day 1

--- Markdowns/Day1.md
+++ Markdowns/Day1.md
@@ -65,3 +65,11 @@ print(type("Abdullah"))
 print(type("Zahid"))
 print(type("Pakistan"))
 ```
+
+<h3>Exercise 2:</h3>
+<p>1. Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.</p>
+
+
+```py
+print("Hello, World!")
+```
---
+++ Programs/Day1/Exercise2.py
@@ -0,0 +1,34 @@
+# Task 1
+print("Task 1:")
+print("Hello, World!")
+print("")
+# Task 2
+print("Task 2:")
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print("")
+
+# Task 3
+print("Task 3:")
+print("Name: ", "Muhammad Abdullah")
+print("Father Name: ", "Zahid Mehmood")
+print("Country: ", "Pakistan")
+print("I am enjoying 30 days of python")
+print("")
+
+# Task 4
+print("Task 4:")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Abdullah','Python','Pakistan']))
+print(type("Abdullah"))
+print(type("Zahid"))
+print(type("Pakistan"))
+print("")
